30cf44a447b0340040871717f2b381314f095651
Ted Juntunen <tedjuntunen@Teddys-Laptop.local>
2018-07-22 20:57:36 -0700

    Change method name readURL()

--- src/Driver.java
+++ src/Driver.java
@@ -8,9 +8,15 @@
 public class Driver {
 
 	public static void main(String[] args) {
-		Ticker ticker = new Ticker("ibm");
+		Ticker ticker = new Ticker("pg");
+		
 		String name = ticker.getStringValueWithKey("companyName");
 		System.out.println(name);
+		
+		System.out.println();
+		
+		double price = ticker.getDoubleValueWithKey("latestPrice");
+		System.out.println(price);
 	}
 
 }
